b02db1e4154de6fe50e39e13f8f8911232e6e869
dbruski <dawidbruski1@gmail.com>
2020-09-03 17:26:29 +0200

    Added modal and months array

---
+++ src/components/molecules/Modal/Modal.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import CloseIcon from '@material-ui/icons/Close';
+
+const StyledOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  backdrop-filter: blur(2px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledWrapper = styled.div`
+  position: relative;
+  width: 50vw;
+  height: 60vh;
+  background: #eee;
+  border-radius: 8px;
+`;
+
+const StyledHeader = styled.div`
+  height: 10vh;
+  width: 100%;
+  background: green;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  color: white;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 20px;
+`;
+
+const StyledCloseButton = styled.button`
+  position: absolute;
+  top: 5vh;
+  right: 5vh;
+  transform-origin: center;
+  transform: translate(50%, -50%);
+  height: 30px;
+  width: 30px;
+  border-radius: 50px;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: red;
+  transition: all 0.2s ease-in-out;
+
+  :hover {
+    transform: translate(50%, -50%) scale(1.19);
+  }
+`;
+
+const StyledContent = styled.div`
+  padding: 20px;
+`;
+
+const Modal = ({ header, children, closeModalFunction }) => {
+  return (
+    <StyledOverlay>
+      <StyledWrapper>
+        <StyledHeader>
+          <h1>{header}</h1>
+        </StyledHeader>
+        <StyledCloseButton onClick={closeModalFunction}>
+          <CloseIcon />
+        </StyledCloseButton>
+        <StyledContent>{children}</StyledContent>
+      </StyledWrapper>
+    </StyledOverlay>
+  );
+};
+
+Modal.propTypes = {
+  header: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+  closeModalFunction: PropTypes.func.isRequired,
+};
+
+export default Modal;
