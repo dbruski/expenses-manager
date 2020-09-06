e1d65eebb1cbdf2845c9886964b1d7d215100286
dbruski <dawidbruski1@gmail.com>
2020-09-06 19:07:22 +0200

    Added input and radio button

---
+++ src/components/atoms/Input/Input.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  font-size: ${({ theme }) => theme.fontSize.m};
+  outline: none;
+  border-radius: 12px;
+  border: 2px solid ${({ theme }) => theme.black};
+  color: ${({ theme }) => theme.black};
+  padding: 5px 10px;
+  transition: 0.2s ease-in-out;
+
+  :focus {
+    border-color: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+export default Input;
---
+++ src/components/atoms/Input/Radio.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledLabel = styled.label`
+  position: relative;
+  padding-left: 25px;
+`;
+
+const StyledCircle = styled.span`
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: block;
+  height: 20px;
+  width: 20px;
+  border: 2px solid ${({ theme }) => theme.black};
+  border-radius: 50px;
+  cursor: pointer;
+
+  ::after {
+    content: '';
+    height: 12px;
+    width: 12px;
+    background: ${({ theme }) => theme.primary};
+    display: block;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%) scale(0);
+    border-radius: 50px;
+    transition: 150ms ease-out;
+  }
+`;
+
+const StyledRadio = styled.input`
+  display: none;
+
+  &:checked {
+    & ~ ${StyledCircle} {
+      border-color: ${({ theme }) => theme.primary};
+    }
+    & ~ ${StyledCircle}::after {
+      transform: translate(-50%, -50%) scale(1);
+    }
+  }
+`;
+
+const Radio = ({ id, name, value }) => {
+  return (
+    <StyledLabel>
+      <StyledRadio type="radio" value={value} id={id} name={name} />
+      {value}
+      <StyledCircle />
+    </StyledLabel>
+  );
+};
+
+export default Radio;
